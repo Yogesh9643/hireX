70a7eb09aa3fb5ae3c8050a874b1a9ab9fb2d1c9
Aniket1298 <171210008@nitdelhi.ac.in>
2023-05-02 21:25:07 +0530

    Added dependencies

--- src/main/java/com/example/Hirex/sql/recruitor.sql
+++ src/main/java/com/example/Hirex/sql/recruitor.sql
@@ -3,6 +3,8 @@ CREATE TABLE `recruiter` (
   `email` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
   `company` varchar(100) NOT NULL,
+   `name` varchar(100) NOT NULL,
+
   PRIMARY KEY (`id`),
   UNIQUE KEY `email_UNIQUE` (`email`)
 )
